f85b08405e170c1b7d414ebd9dd7f65716a4e4ee
Ernest C <Ernest C>
2017-09-28 12:36:18 -0400

    got question displaying

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,15 @@
-var questionsArrary = ["<p>What is the first element on the periodic table?<br><input type='radio' name='test' value='1' checked>One &nbsp; <input type='radio' name='test' value='2'>Hydrogen &nbsp; <input type='radio' name='test' value='3'>Oxygen &nbsp; <input type='radio' name='test' value='4'>Helium</p>", "<p>The symbol ‘Pb’ refers to which chemical element?</p>",	"<p>What mineral is also referred to as \"Fool's gold\"?</p>", "<p>What was the first element to be made artificially</p>", "<p>How many elements on the periodic table that occur in nature.</p>"]
+	// {
+	// 	question:"What is the first element on the periodic table?",
+	// 	answer:2,
+	// 	options: ["Barium","Oxygen","Hydrogen","Helium"]
+	// },
+var questionsArrary = [
+	"<p>What is the first element on the periodic table?<br><input type='radio' name='question1' value='1' checked>One &nbsp; <input type='radio' name='question1' value='2'>Hydrogen &nbsp; <input type='radio' name='question1' value='3'>Oxygen &nbsp; <input type='radio' name='question1' value='4'>Helium</p>", 
+	"<p>The symbol ‘Pb’ refers to which chemical element?<br><input type='radio' name='question2' value='1' checked>Phosphorus &nbsp; <input type='radio' name='question2' value='2'>Iron &nbsp; <input type='radio' name='question2' value='3'>Lead &nbsp; <input type='radio' name='question2' value='4'>Polonium</p>",	
+	"<p>What mineral is also referred to as \"Fool's gold\"?<br><input type='radio' name='question3' value='1' checked>Phosphorus &nbsp; <input type='radio' name='question3' value='2'>Pyrite &nbsp; <input type='radio' name='question3' value='3'>Polonium &nbsp; <input type='radio' name='question3' value='4'>Lead</p>", 
+	"<p>What was the first element to be made artificially<br><input type='radio' name='question4' value='1' checked>Phosphorus &nbsp; <input type='radio' name='question4' value='2'>Iron &nbsp; <input type='radio' name='question4' value='3'>Polonium &nbsp; <input type='radio' name='question4' value='4'>Lead</p>", 
+	"<p>How many elements on the periodic table that occur in nature?<br><input type='radio' name='question5' value='1' checked>Phosphorus &nbsp; <input type='radio' name='question5' value='2'>Iron &nbsp; <input type='radio' name='question5' value='3'>Polonium &nbsp; <input type='radio' name='question5' value='4'>Lead</p>"
+	]
 var outputQuestions = [];
 var answersArray = ["Hydrogen", "Lead", "Pyrite", "Technetium", "90"]
 var startScreen;
